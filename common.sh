f89bcce8bcb5d8c33dfd7e437bd6ed42dfc0f37e
manishamaidam <maidammanisha.gmail.com>
2025-03-23 20:50:38 +0530

    sourcing code form other script

--- common.sh
+++ common.sh
@@ -10,8 +10,8 @@ trap 'handle_error ${LINENO} "$BASH_COMMAND"' ERR
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f2)
-LOGFILE=/tmp/$SCRIPT_NAME_$TIMESTAMP.log
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
--- mysql.sh
+++ mysql.sh
@@ -15,10 +15,9 @@ systemctl start mysqld &>>$LOGFILE
 
 #Below code will be useful for idempotent nature
 mysql -h db.ramani280791.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
-
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
 else
-    echo -e "MySQL Root password is already setup... $Y SKIPPING $N"
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
